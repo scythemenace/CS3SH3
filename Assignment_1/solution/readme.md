6ba70dd15fa21d3aeaa6f82622b70cddf09f50f3
scythemenace <pankur216@gmail.com>
2024-09-19 23:11:34 +0000

    Ammended the final changes

---
+++ Assignment_1/solution/readme.md
@@ -0,0 +1,5 @@
+## Work allocated:
+
+Me and my lab partner worked on this assignment together so there is no way to quantify what work we did since we did it simultaneously.
+We deduced explanations for the sample file that was provided together on our own and wrote the code side by side. Therefore, according
+to our arbitrary calculation, the work was split 50-50 between us.
--- Assignment_1/solution/seconds.c
+++ Assignment_1/solution/seconds.c
@@ -25,7 +25,8 @@ ssize_t proc_read(struct file *file, char *buf, size_t count, loff_t *pos);
 int proc_init(void);
 void proc_exit(void);
 
-// Global variables for start_time and end_time
+// Global variables for showcasing start_time and end_time, by time we mean the jiffies which will give us the time when divided
+// by the HZ rate.
 unsigned long start_jiffies;
 unsigned long later_jiffies;
 
@@ -41,6 +42,8 @@ int proc_init(void)
         // the following function call is a wrapper for
         // proc_create_data() passing NULL as the last argument
         proc_create(PROC_NAME, 0, NULL, &my_proc_ops);
+        
+        /*Since proc_init is called when the module first loads, we are initializing the value for our start_jiffies here*/
         start_jiffies = jiffies;
 
         printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
@@ -75,7 +78,6 @@ void proc_exit(void) {
 ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
 {
         int rv = 0;
-        char buffer[BUFFER_SIZE];
         static int completed = 0;
 
         if (completed) {
@@ -85,17 +87,23 @@ ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t 
 
         completed = 1;
         
+        /* Basically whenever someone will write cat /proc/seconds, a new value will be assigned
+          to the later_jiffies variables so that we can have the current jiffies value since the kernel
+          was booted
+         */
         later_jiffies = jiffies;
-        unsigned long difference_in_jiffies = later_jiffies - start_jiffies;
+        unsigned long difference_in_jiffies = later_jiffies - start_jiffies; // Denotes the difference in jiffies from current to the value assigned when kernel was booted
         unsigned long elapsed_time = difference_in_jiffies/HZ;
-        char elapsed_time_in_char[BUFFER_SIZE]; 
-        snprintf(elapsed_time_in_char, sizeof(elapsed_time_in_char), "%lu", elapsed_time);
-
-        rv = sprintf(buffer, "%s\n", elapsed_time_in_char);
-
+        /*The line below declares a character array (elapsed_time_in_char) of size buffer size. This array will be used to store
+         the formatted string representation of the elapsed time (in seconds) that has passed since the kernel module was loaded.*/
+        char elapsed_time_in_char[BUFFER_SIZE];
+        /*The (snprintf) function writes the value of elapsed_time into the elapsed_time_in_char array, formatted as a string
+         using the "%lu" format specifier (used for types unsigned long).*/
+        rv = snprintf(elapsed_time_in_char, sizeof(elapsed_time_in_char), "Seconds elapsed: %lu\n", elapsed_time); 
+ 
         // copies the contents of buffer to userspace usr_buf
-        copy_to_user(usr_buf, buffer, rv); //This shows a warning as copy_to_user retrns 0 for success and we aren't allocating it,
-        // because there's no use for it
+        // This shows a warning as copy_to_user returns 0 for success and we aren't allocating it so it can be ignored
+        copy_to_user(usr_buf, elapsed_time_in_char, rv); 
 
         return rv;
 }
