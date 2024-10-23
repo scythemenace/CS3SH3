4c75a57823990148db885f43a5425504845216cd
Ankur Pandey <pankur216@gmail.com>
2024-10-23 04:27:37 -0400

    Fixed part 2 errors

--- Lab_3/solution/part2.c
+++ Lab_3/solution/part2.c
@@ -186,15 +186,6 @@ void *deposit(void *param)
   {
     amount += deposit_amount;
   }
-  else
-  {
-    ret = sem_post(&above_limit);
-    if (ret != 0)
-    {
-      printf("sem_post failed");
-    }
-    pthread_exit(NULL);
-  }
 
   if (amount > 0)
   {
