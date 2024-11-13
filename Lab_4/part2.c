6a37c3a7a3ecf93823e78e4f10c04f09f6974420
scythemenace <pankur216@gmail.com>
2024-11-13 09:59:43 +0000

    Init part2

--- Lab_4/part2.c
+++ Lab_4/part2.c
@@ -1,3 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
+#define INT_SIZE 4
+#define INT_COUNT 10
+#define MEMORY_SIZE (INT_SIZE * INT_COUNT)
+
+int main() {
+  int intArray[INT_COUNT];
+  signed char *mmapfptr;
+
+  int mmapfile_fd = open("numbers.bin", 0_RDONLY);
+  if (mmapfile_fd == -1) {
+    perror("Error opening file");
+    exit(EXIT_FAILURE);
+  }
+
+  mmapfptr = mmap(NULL, MEMORY_SIZE, PROT_READ, MAP_PRIVATE, mmapfile_fd, 0);
+
+  for (int i = 0; i < INT_COUNT; i++) {
+    memcpy(&intArray[i], mmapfptr + INT_SIZE * i, INT_SIZE);
+  }
+
+  munmap(mmapfptr, MEMORY_SIZE);
+
+  close(mmapfile_fd);
+
+  int sum = 0;
+  
+  for (int i = 0; i < INT_COUNT; i++) {
+    sum += intArray[i];
+  }
+
+  printf("Sum of numbers: %d\n", sum);
+
+  return 0;
+}
